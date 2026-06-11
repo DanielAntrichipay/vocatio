e23eb62e26762be98424ab7d84ca1c5b53c74e4f
daniel <danielantrichipay20@gmail.com>
2026-06-11 19:00:38 -0300

    Commit inicial

---
+++ src/main/java/com/protype/vocatio/VocatioApplication.java
@@ -0,0 +1,13 @@
+package com.protype.vocatio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocatioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocatioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/protype/vocatio/VocatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protype.vocatio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocatioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
